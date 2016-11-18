0f638c8dd7979bb26360b45579fb6ba680cea402
Gonçalo Cardoso <goncalo.cb.ferreira@gmail.com>
2016-11-18 00:02:08 +0000

    First commit - added cron job to check code school current prices

---
+++ index.js
@@ -0,0 +1,47 @@
+var CronJob = require('cron').CronJob;
+var http = require('http-request');
+var HTMLParser = require('fast-html-parser');
+var SlackWebhook = require('slack-webhook');
+var slack = new SlackWebhook('https://hooks.slack.com/services/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');
+
+var getPriceFromHTML = function ( htmlParsed, selector ) {
+
+	var price = null;
+	var obj = htmlParsed.querySelector(selector);
+
+	if ( obj && obj.childNodes ){
+		price = obj.childNodes[ 0 ].rawText;
+	}
+
+	return price;
+};
+
+var getCodeSchoolPrices = function() {
+
+	http.get('https://www.codeschool.com/pricing', function( err, data ){
+
+		if ( !err ){
+			var html = data.buffer.toString();
+			var htmlParsed = HTMLParser.parse(html);
+			var yearlyPrice = getPriceFromHTML(htmlParsed, '.js-price-target');
+			var monthlyPrice = getPriceFromHTML(htmlParsed, '#monthly-price');
+
+			slack.send({
+				text: '@tiago @gcardoso @bruno.assuncao @claudia_bernardo @ricardo.proenca @goncalo.assuncao @iranha Code school prices: *' + yearlyPrice + '*$/year | *' + monthlyPrice + '*$/month',
+				icon_emoji: ':ghost:',
+				link_names: 1
+			})
+		}
+
+	});
+
+};
+
+var job = new CronJob({
+	cronTime: '00 * 23 * * 0-6',
+	onTick: getCodeSchoolPrices,
+	start: false,
+	timeZone: 'Europe/Lisbon'
+});
+
+job.start();
