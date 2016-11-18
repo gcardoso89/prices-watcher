aafebc5857a8d5bdcb194f6aac5dc141a98ddb89
Gonçalo Cardoso <goncalo.cb.ferreira@gmail.com>
2016-11-18 19:45:52 +0000

    Implemented 6month plan thing

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ var slack = new SlackWebhook('https://hooks.slack.com/services/T0453AP6B/B349CC6
 
 var currentYear = null;
 var currentMonth = null;
+var current6Month = null;
 
-var getPriceFromHTML = function ( htmlParsed, selector ) {
+var getPriceFromHTML = function ( htmlParsed, selector, index ) {
 
 	var price = null;
-	var obj = htmlParsed.querySelector(selector);
+	index = index | 0;
+	var obj = htmlParsed.querySelectorAll(selector)[ index ];
 
-	if ( obj && obj.childNodes ){
+	if ( obj && obj.childNodes && obj.childNodes[ 0 ] ){
 		price = obj.childNodes[ 0 ].rawText;
 	}
 
@@ -27,18 +29,33 @@ var getCodeSchoolPrices = function() {
 
 			var html = data.buffer.toString();
 			var htmlParsed = HTMLParser.parse(html);
-			var yearlyPrice = getPriceFromHTML(htmlParsed, '.js-price-target');
+			var sixMonthPrice = getPriceFromHTML(htmlParsed, '.js-price-target');
 			var monthlyPrice = getPriceFromHTML(htmlParsed, '#monthly-price');
+			var yearlyPrice = getPriceFromHTML(htmlParsed, '.js-price-target', 1);
+
+			yearlyPrice = yearlyPrice ? yearlyPrice : sixMonthPrice;
+
+			if ( !yearlyPrice ){
+				yearlyPrice = sixMonthPrice;
+				sixMonthPrice = null;
+			}
 
 			if ( yearlyPrice !== currentYear || monthlyPrice !== currentMonth ){
 				slack.send({
-					text: '@tiago @gcardoso @bruno.assuncao @claudia_bernardo @ricardo.proenca @goncalo.assuncao @iranha NEW CODE SCHOOL PRICES!!!\n*' + yearlyPrice + '*$/year | *' + monthlyPrice + '*$/month\nhttps://www.codeschool.com/pricing',
+					text: '@tiago @gcardoso @bruno.assuncao @claudia_bernardo @ricardo.proenca @goncalo.assuncao @iranha'+
+					'NEW CODE SCHOOL PRICES!!!\n'+
+					'*Yearly plan:* $' + yearlyPrice + '/mo ($'+ parseInt( yearlyPrice, 10 ) * 12 + ')' +
+					'\n*Montly plan:* $' + monthlyPrice + '/mo' +
+					( sixMonthPrice ? '\n*6-Monthly plan:* $' + sixMonthPrice + '/mo ($'+ parseInt( yearlyPrice, 10 ) * 6 + ')' : '\n' ) +
+					'\nhttps://www.codeschool.com/pricing',
 					icon_emoji: ':ghost:',
+					username: 'Dragon Bot',
 					link_names: 1
 				});
 
 				currentYear = yearlyPrice;
 				currentMonth = monthlyPrice;
+				current6Month = sixMonthPrice;
 			}
 
 		} else {
